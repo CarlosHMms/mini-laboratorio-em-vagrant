e6e95c0b75ceedf58fafc6c11eaa15c2180421fd
CarlosHMms <carloshmmg2@gmail.com>
2023-09-17 15:08:52 -0300

    Correções e Comentários

--- shared_folder/vm_cfg/cfg_vm1.sh
+++ shared_folder/vm_cfg/cfg_vm1.sh
@@ -1,5 +1,8 @@
-      apt-get update
-      apt-get -y install net-tools
-      
-      sudo sysctl -w net.ipv4.ip_forward=1
-      sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+#!/bin/bash
+#Instalando as ferramentas de desenvolvimento
+apt-get update
+apt-get -y install net-tools
+
+#Configurando a rede, ativando o ip_forward e configurando as regras de NAT
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
--- shared_folder/vm_cfg/cfg_vm2.sh
+++ shared_folder/vm_cfg/cfg_vm2.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+#Instalando as ferramentas de desenvolvimento
 apt-get update
 apt-get install -y apache2
 apt-get install -y net-tools
 
+#Configurando a rede e definindo o gateway
 sudo ip route del default
 sudo route add default gw 192.168.56.254
 sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
